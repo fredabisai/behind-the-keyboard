8161ab5db57b6cc604dea76a6f9a3aa72fb11e45
Fredrick <fredabisai@gmail.com>
2019-04-23 23:16:03 +0300

    Admin Login interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminLoginComponent } from './login/admin-login/admin-login.component';
+import { ExpertLoginComponent } from './login/expert-login/expert-login.component';
+import { UserLoginComponent } from './login/user-login/user-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    children: [
+        {
+            path: 'admin',
+            component: AdminLoginComponent
+        },
+        {
+            path: 'expert',
+            component: ExpertLoginComponent
+        },
+        {
+            path: 'user',
+            component: UserLoginComponent
+        }
+    ]
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
